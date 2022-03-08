daa681b9757ddc2596da142d2160afea93db56f8
Jovan.Filiposki <Jovan.Filiposki@PALMYRA15>
2022-03-08 20:30:05 +0100

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Class1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*Console.WriteLine("Hello World!");
+            Console.WriteLine("Zdravo Group2 !");
+            Console.WriteLine(1);
+            Console.WriteLine("1");
+            Console.WriteLine("Ova e poraka vo \n nov red\n");*/
+
+            /*Console.WriteLine("   *");
+            Console.WriteLine("  ***");
+            Console.WriteLine(" *****");
+            Console.WriteLine("*******");*/
+
+            Console.WriteLine("    *\n   ***\n  *****\n *******");
+
+            Console.WriteLine("Enter your text");
+            var input = Console.ReadLine();
+            Console.WriteLine(input);
+            
+        }
+    }
+}       
